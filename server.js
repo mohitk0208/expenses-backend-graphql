0f20e791a74f955615a09a1cc1799adcd1a5547b
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-12-30 23:30:51 +0530

    connect to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const passport = require("passport")
 
 dotenv.config()
 
@@ -12,8 +14,18 @@ app.get("/", (req, res) => {
   res.send("hello world")
 })
 
-const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`)
-})
+
+
+const PORT = process.env.PORT || 4000
+mongoose
+  .connect(`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`, {}
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+  })
