10114efcb464f095197b0a79869750976d6c0118
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-01-01 23:17:35 +0530

    .

--- models/expense.js
+++ models/expense.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose")
 const expenseSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   amount: { type: Number, required: true },
-  spentOn: String,
+  spentOn: { type: String },
   category: { type: mongoose.Types.ObjectId, required: true, ref: "Category" },
   month: { type: mongoose.Types.ObjectId, required: true, ref: "Month" },
-  user: {type: mongoose.Types.ObjectId, required: true, ref: "User"}
+  user: { type: mongoose.Types.ObjectId, required: true, ref: "User" }
 })
 
 module.exports = mongoose.model("Expense", expenseSchema)
